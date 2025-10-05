b0411c34d483f1a9aa8d9f39f3506fbca563af47
Marco Kuoni <mail@marcokuoni.ch>
2025-10-05 16:16:25 +0200

    give VITE_SERVER also to socket

--- web/src/lib/socket.ts
+++ web/src/lib/socket.ts
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client";
-export const socket = io(
-  (window as any).__SERVER__ ?? "http://localhost:3001",
-  {
-    transports: ["websocket"],
-  },
-);
+
+const base =
+  (window as any).__SERVER__ ||
+  import.meta.env.VITE_SERVER ||
+  "http://localhost:3001";
+
+export const socket = io(base, { transports: ["websocket"] });
